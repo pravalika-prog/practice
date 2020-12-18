73a6e59d976de6c19afdab08bd91e301e99b3770
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-18 19:42:47 +0530

    Add files via upload

---
+++ inheritance/laptop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ienumerable
+{
+
+
+    class laptop : laptop1
+    {
+        public void Showdetails(string Name, string Price, string Processor, string ram)
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Price : " + Price);
+            Console.WriteLine("Processor:" + Processor);
+            Console.WriteLine("ram:" + ram);
+        }
+    }
+}
+
+    
+       
+ //   class program : baselaptop
+ //   {
+ //       public void Showdetails(string name, string price, string ram, string processor);
+ //       {
+ //           Console.WriteLine("Name : "+name);
+ //           Console.WriteLine("Price : " + price);
+ //           Console.WriteLine("Processor:"+processor);
+ //Console.WriteLine("ram:"+ram);
+ //       }
+ //   }
+   
+
+
---
+++ inheritance/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ienumerable
+{
+    class program
+    {
+        static void Main()
+        {
+            laptop lp = new laptop();
+            lp.Showdetails(laptop.Lenovo, laptop.price1000, laptop.Ram2GB, laptop.i3);
+            Console.WriteLine("**************************************");
+            lp.Showdetails(laptop.dell, laptop.price1500, laptop.Ram4GB, laptop.i5);
+            Console.WriteLine("**************************************");
+            lp.Showdetails(laptop.hp, laptop.price2500, laptop.Ram8GB, laptop.i7);
+            Console.WriteLine("**************************************");
+        }
+    }
+}
+
