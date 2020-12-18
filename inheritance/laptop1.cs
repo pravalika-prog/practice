0655ad07eb74f0c33b8780f33e1368619945895b
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-18 19:41:19 +0530

    Create laptop1.cs

---
+++ inheritance/laptop1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ienumerable
+{
+    class laptop1
+    {
+        public const string Lenovo = "lenovo";
+        public const string dell = "dell";
+        public const string hp = "hp";
+        public const string price1000 = "1000";
+        public const string price1500 = "1500";
+        public const string price2500 = "2500";
+        public const string Ram2GB = "2GB";
+        public const string Ram4GB = "4GB";
+        public const string Ram8GB = "8GB";
+        public const string i3 = "i3";
+        public const string i5 = "i5";
+        public const string i7 = "i7";
+
+    }
+}
